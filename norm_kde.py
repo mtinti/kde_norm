8aff26ff702b10c5655bd49c95a11d35507ac26d
mtinti <michele.tinti@gmail.com>
2017-06-07 13:40:08 +0100

    add test with pandas

--- norm_kde.py
+++ norm_kde.py
@@ -3,12 +3,17 @@
 Created on Wed Jun 07 12:48:48 2017
 
 @author: mtinti-x
+
+resource:
+http://seaborn.pydata.org/generated/seaborn.kdeplot.html
+http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.plot.kde.html
 """
 
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.style.use('ggplot')
 import numpy as np
+
 
 
 #this function get a kde plot x and y values
@@ -26,7 +31,20 @@ def make_norm_kde(ax_old, ax_new):
     ax_new.set_ylim(0, 1.1)
     
 
+#test with pandas
 def test_1():
+    import pandas as pd
+    dataset_1 = np.random.normal(size=100, loc=2, scale=1.0)
+    dataset_2 = np.random.normal(size=100, loc=5, scale=5.0)
+    df = pd.DataFrame()
+    df['dataset_1']=dataset_1
+    df['dataset_2']=dataset_2
+    fig,ax = plt.subplots()
+    df.plot(kind='kde',ax=ax)
+    plt.show()
+    fig2,ax2 = plt.subplots()
+    make_norm_kde(ax, ax2)
+    plt.show()
     pass
 
 
