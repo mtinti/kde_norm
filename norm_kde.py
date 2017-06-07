4473a447737aab5ca3f6524467619c27e86ba5bd
mtinti <michele.tinti@gmail.com>
2017-06-07 13:16:34 +0100

    start point

---
+++ norm_kde.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 07 12:48:48 2017
+
+@author: mtinti-x
+"""
+
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.style.use('ggplot')
+import numpy as np
+
+
+#this function get a kde plot x and y values
+#and crete a new kde plot normalized with the
+#max values
+def make_norm_kde(ax_old, ax_new):
+    max_points = []
+    for item in enumerate(ax_old.lines):
+        line = ax_old.lines[item[0]]
+        max_points.append(line.get_ydata().max())
+    max_points = np.array(max_points)    
+    for item in enumerate(ax_old.lines):
+        line = ax_old.lines[item[0]]
+        ax_new.plot(line.get_xdata(), line.get_ydata()/max_points.max() )
+    ax_new.set_ylim(0, 1.1)
+    
+
+def test_1():
+    pass
+
+
+if __name__ == '__main__':
+    test_1()
